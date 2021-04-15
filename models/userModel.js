e55cd2f84d6cbf40ba37f8ac4ce2c98ef6cdbfaa
aniro45 <aniket.jadhav.8151@gmail.com>
2021-04-16 01:07:56 +0530

    added fully functioned model with all field inscluding

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('mongoose-type-url');
 
 const UserSchema = new mongoose.Schema({
   fname: {
@@ -52,9 +53,97 @@ const UserSchema = new mongoose.Schema({
     },
   },
 
+  username: {
+    type: String,
+    required: [true, 'Please provide the username'],
+    unique: true,
+    lowercase: true,
+    minlength: [2, 'username should be of at least 2 characters!'],
+    maxlength: [17, 'username should not have more than 17 characters!'],
+  },
+
+  gender: {
+    type: String,
+    enum: {
+      values: ['male', 'female', 'other'],
+      message: 'user must be among following, male, female or other!',
+    },
+  },
+
+  headline: {
+    type: String,
+    maxlength: [18, 'headline should not be greater than 18 characters!'],
+  },
+
+  bio: {
+    type: String,
+    maxlength: [400, 'bio should not be grater than 150 characters!'],
+    trim: true,
+  },
+
+  profilePic: {
+    type: String,
+    default: 'default.jpeg',
+  },
+
+  coverPic: {
+    type: String,
+    default: 'defaultbg.jpeg',
+  },
+
+  socialLinks: [
+    {
+      type: mongoose.SchemaTypes.Url,
+    },
+  ],
+
+  location: {
+    type: String,
+    maxlength: [25, 'location should not be greater than 25 characters!'],
+    minLength: [1, 'location must be at least of 1 character!'],
+    trim: true,
+  },
+
+  skills: [
+    {
+      type: String,
+      trim: true,
+    },
+  ],
+
+  password: {
+    type: String,
+    required: [true, 'user must provide password!'],
+    minlength: [8, 'Passowrd must be of at least 8 characters'],
+    select: false,
+  },
+
+  passwordConfirm: {
+    type: String,
+    required: [true, 'user must provide confirm password field!'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'passwords are not the same!',
+    },
+    select: false,
+  },
+
+  passwordChangedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+
   createdAt: {
     type: Date,
     default: Date.now(),
+    select: false,
+  },
+
+  active: {
+    type: Boolean,
+    default: true,
     select: false,
   },
 });
